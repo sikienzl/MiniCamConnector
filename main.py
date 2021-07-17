7bd5e681c6dc87e12a9a99ecd0563f64f9ea6704
Siegfried Kienzle <siegfried.kienzle@gmx.de>
2021-07-17 19:02:39 +0200

    refactoring of classes
    add a menu structure on main-class
    and also modify the methods itself.

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 from wifiInformation import WifiInformation
 
-
 if __name__ == '__main__':
     wifiInfo = WifiInformation()
     wifiInterfaces = wifiInfo.getInterfaces()
+
     for interface_count in range(0, len(wifiInterfaces)):
-        print(str(wifiInterfaces[interface_count]))
-    result_ap_list = wifiInfo.getAPs(0)
+        print(str(interface_count) + ": " + str(wifiInterfaces[interface_count]))
+    device = 0
+    device_not_selected = True
+    while device_not_selected:
+        device = int(input("Select one of these devices: "))
+        if device < len(wifiInterfaces):
+            device_not_selected = False
+
+    print("Scan of AP's starts now:")
+    result_ap_list = wifiInfo.getAPs(device)
     for ap_count in range(0, len(result_ap_list)):
         print(str(ap_count) + ": " + result_ap_list[ap_count])
 
+    ap = 0
+    ap_not_selected = True
+
+    while ap_not_selected:
+        ap = int(input("Select one of these AP's: "))
+        if ap < len(result_ap_list):
+            ap_not_selected = False
+
+    ap_ssid = result_ap_list[ap]
+    print("Wifi encryption: " + wifiInfo.get_key_management_mode_of_ssid(ap_ssid))
--- wifiInformation.py
+++ wifiInformation.py
@@ -3,21 +3,65 @@ import time
 from converter import Converter
 
 class WifiInformation:
+    NONE = "None"
+    WPA = "WPA"
+    WPAPSK = "WPA PSK"
+    WPA2 = "WPA2"
+    WPA2PSK = "WPA2 PSK"
+    UNKNOWN = "Unknown"
+
     def getInterfaces(self):
         wifi = pywifi.PyWiFi()
         converter = Converter()
         strInterfaces = converter.convertIntoStringArry(wifi.interfaces())
         return strInterfaces
 
+    """def __init__(self, ap_results):
+        self.__ap_results = ap_results"""
+
+    def __get_ap_results(self):
+        return  self.__ap_results
+
+    def __set_ap_results(self, ap_results):
+        self.__ap_results = ap_results
+
     def getAPs(self, wifiInterfacNumber):
         wifi = pywifi.PyWiFi()
         iface = wifi.interfaces()[wifiInterfacNumber]
         iface.scan()
         time.sleep(5)
-        ap_results= iface.scan_results()
+        ap_results = iface.scan_results()
+        self.__set_ap_results(ap_results)
         aps_result_list = {}
 
         for ap_results_number in range(0, len(ap_results)):
             aps_result_list[ap_results_number] = ap_results[ap_results_number].ssid
 
         return aps_result_list
+
+    def get_key_management_mode_of_ssid(self, ssid):
+        ap_results = self.__get_ap_results()
+        key_management_mode = ""
+        for ap_results_number in range(0, len(ap_results)):
+            if ap_results[ap_results_number].ssid == ssid:
+                for key_management_value in ap_results[ap_results_number].akm:
+                    key_management_mode = self.__get_key_management_mode_from_value(key_management_value)
+        return key_management_mode
+
+    def __get_key_management_mode_from_value(self, key_management_value):
+        key_management_mode = ""
+        if key_management_value == 0:
+            key_management_mode = self.NONE
+        elif key_management_value == 1:
+            key_management_mode = self.WPA
+        elif key_management_value == 2:
+            key_management_mode = self.WPAPSK
+        elif key_management_value == 3:
+            key_management_mode = self.WPA2
+        elif key_management_value == 4:
+            key_management_mode = self.WPA2PSK
+        else:
+            key_management_mode = self.UNKNOWN
+        return key_management_mode
+
+
